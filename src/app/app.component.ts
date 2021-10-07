05ca6027ea8f14baed7b3a7c801e971be6f7ac78
Konrad <konradarthas@gmail.com>
2021-10-07 14:29:51 +0200

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,109 @@
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Component, Input } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpService } from './http.service';
+import { Post } from './modules/post';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent  {
+  title = 'angular-http';
+
+  constructor(private httpService: HttpService) { }
+
+
+  myPosts: Array<Post> = [];
+  getPostText?: number;
+  getPostsByUserIdText?: number;
+  allPosts$!: Observable<Array<Post>>;
+  //allPosts$: Observable<Post[]> = of([]);
+
+  getPosts() {
+    this.httpService.getPosts().subscribe(posts => {
+      this.myPosts = posts;
+      console.log(posts);
+    });
+    this.allPosts$ = this.httpService.getPosts();
+  }
+
+  getPost() {
+    if (this.getPostText == null) {
+      return;
+    }
+    this.httpService.getPost(this.getPostText).subscribe(post => {
+      this.myPosts.push(post);
+      console.log(post);
+    });
+  }
+
+  getPostsByUserId() {
+    if (this.getPostsByUserIdText == null) {
+      return;
+    }
+    this.httpService.getPostsByUserId(this.getPostsByUserIdText).subscribe(posts => {
+      this.myPosts = posts;
+      console.log(posts);
+    });
+  }
+
+  addPost() {
+    const myPost = this.getNewPost();
+    this.httpService.addPost(myPost).subscribe(post => {
+      this.myPosts.push(post);
+      console.log(post);
+    });
+  }
+
+  updatePost() {
+    const myPost = this.getNewPost();
+    this.httpService.updatePost(myPost).subscribe(post => {
+      this.myPosts.push(post);
+      console.log(post);
+    });
+  }
+
+  changePost() {
+    const myPost: Post = ({
+      id: 1,
+      body: 'Mój pierwszy post'
+    });
+    this.httpService.changePost(myPost).subscribe(post => {
+      this.myPosts.push(post);
+      console.log(post);
+    });
+  }
+
+  deletePost() {
+    this.httpService.deletePost(1).subscribe(post => {
+      console.log(post);
+    });
+  }
+
+  getPostsWithError() {
+    this.httpService.getPostsWithError().subscribe(posts => {
+      console.log(posts);
+    },
+    (error: HttpErrorResponse) => {
+      console.log(error.status);
+    });
+  }
+
+  getNewPost(): Post {
+    const post: Post = ({
+      userId: 1,
+      id: 1,
+      title: 'Mój post',
+      body: 'Mój pierwszy post'
+    });
+    return post;
+  }
+
+  getwWeatherforecast() {
+    this.httpService.getwWeatherforecast().subscribe(posts => {
+      console.log(posts);
+    });
+  }
+}
---
+++ src/app/http.service.ts
@@ -0,0 +1,50 @@
+import { Post } from './modules/post';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http: HttpClient) { }
+
+  getPosts(): Observable<Array<Post>> {
+    return this.http.get<Array<Post>>('https://jsonplaceholder.typicode.com/posts');
+  }
+
+  getPost(id: number): Observable<Post> {
+    return this.http.get<Post>('https://jsonplaceholder.typicode.com/posts/'+id);
+  }
+
+  getPostsByUserId(userId: number): Observable<Array<Post>> {
+    const httpParams = new HttpParams().set('userId', userId.toString());
+    return this.http.get<Array<Post>>('https://jsonplaceholder.typicode.com/posts?userId=', { params: httpParams });
+  }
+
+  addPost(post: Post): Observable<Post> {
+    return this.http.post<Post>('https://jsonplaceholder.typicode.com/posts/', post);
+  }
+
+  updatePost(post: Post): Observable<Post> {
+    return this.http.put<Post>('https://jsonplaceholder.typicode.com/posts/' + post.id, post);
+  }
+
+  changePost(post: Post): Observable<Post> {
+    return this.http.patch<Post>('https://jsonplaceholder.typicode.com/posts/' + post.id, post);
+  }
+
+  deletePost(id: number): Observable<Post> {
+    return this.http.delete<Post>('https://jsonplaceholder.typicode.com/posts/'+id);
+  }
+
+  getPostsWithError(): Observable<Array<Post>> {
+    return this.http.get<Array<Post>>('https://jsonplaceholder.typicode.com/xyxz');
+  }
+
+
+  getwWeatherforecast(): Observable<any> {
+    return this.http.get<any>('http://konradtest.aspnet.pl/weatherforecast');
+  }
+}
